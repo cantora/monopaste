450f5c50509613ea71a323b7cb7e201c3dcd5a4d
anthony cantor <ar.cantor@gmail.com>
2014-05-11 02:15:48 -0700

    track buffer changes using tmux command line client

--- lib/overpaste/adapters/tmux-cli.rb
+++ lib/overpaste/adapters/tmux-cli.rb
@@ -3,7 +3,9 @@
   'receivesbuffers',
   'originatesbuffers',
   'logs',
-  'adapter'
+  'adapter',
+  'buffer',
+  'timestamp'
 ].each {|x| require(File.join('overpaste', x)) }
 
 module Overpaste
@@ -15,9 +17,21 @@ Adapter::define_adapter_for('tmux-cli') do
   include Logs
 
   set_conf_default('poll_interval', 500)
-    
-  to_poll do
-    logger.debug("tmux poll")
+
+  to_poll do |itr|
+    @buf_history ||= []
+    buf = `tmux show-buffer`
+    ts = Time.now
+
+    #logger.debug("buf(#{itr}): #{buf[0..31].inspect}")
+    if !buf.nil? && !buf.empty? \
+        && (@prev_buf.nil? || buf != @prev_buf)
+      stamp = Timestamp.new(ts.to_i, ts.tv_usec)
+      record = Buffer.new(stamp, buf)
+      logger.info("got buffer from tmux at #{stamp.inspect}")
+      @buf_history << record
+      @prev_buf = buf
+    end
   end
 
 end
